14652876f5cff675ed9b79b9f2e07cc11fded6a1
Ната <natavts@gmail.com>
2018-11-08 17:26:38 +0200

    eslint + separation of the visual part from the core

--- src/App.js
+++ src/App.js
@@ -7,7 +7,47 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Calculate commission={1.2} price={1.2}/>
+        <Calculate
+          commission={1.2}
+        >
+          {(calcProps) => {
+            const {
+              price,
+              commission,
+              actions,
+              amount,
+              handlePrice,
+              handleCommission,
+              handleActions,
+              handleAmount,
+            } = calcProps;
+            return (
+              <div className="Calculate">
+                <form>
+                  <label>Price:
+                    <input className="price" type="number" value={price} onChange={handlePrice} />
+                  </label>
+                  <label>Price with commission:
+                    <input className="commission" type="number" value={commission} onChange={handleCommission} />
+                  </label>
+                  <label>Actions:
+                    <input className="action" type="number" value={actions} onChange={handleActions} />
+                  </label>
+                  <label>Total:
+                    <input className="total" type="number" value={amount} onChange={handleAmount} />
+                  </label>
+                </form>
+
+                <div className="general-state">
+                  price: {price} <br />
+                  comission: {commission} <br />
+                  actions: {actions / 1000} <br />
+                  amount: {amount}
+                </div>
+              </div>
+            );
+          }}
+        </Calculate>
       </div>
     );
   }
--- src/Calculate/Calculate.js
+++ src/Calculate/Calculate.js
@@ -1,71 +1,70 @@
 import React, { Component } from 'react';
 
 class Calculate extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            price: '',
-            commission: '',
-            actions: '1000',
-            amount: ''
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: '',
+      commission: '',
+      actions: '1000',
+      amount: '',
+      ...props.initialValues,
+    };
 
-        this.handlePrice = this.handlePrice.bind(this);
-        this.handleCommission = this.handleCommission.bind(this);
-        this.handleActions = this.handleActions.bind(this);
-        this.handleAmount = this.handleAmount.bind(this);
-    }
+    this.getCalculateProps = this.getCalculateProps.bind(this);
+    this.handlePrice = this.handlePrice.bind(this);
+    this.handleCommission = this.handleCommission.bind(this);
+    this.handleActions = this.handleActions.bind(this);
+    this.handleAmount = this.handleAmount.bind(this);
+  }
 
-    handlePrice(e) {
-        let price = e.target.value,
-            commission = this.props.commission * e.target.value,
-            amount = commission * this.state.actions / 1000;
-        this.setState({price: price, commission: commission.toFixed(2), amount: amount.toFixed(2)});
-    }
+  getCalculateProps() {
+    return {
+      ...this.state,
+      handlePrice: this.handlePrice,
+      handleCommission: this.handleCommission,
+      handleActions: this.handleActions,
+      handleAmount: this.handleAmount,
+    };
+  }
 
-    handleCommission(e) {
-        let commission = e.target.value,
-            price = e.target.value / this.props.price,
-            amount = commission * this.state.actions / 1000;
-        this.setState({commission: commission, price: price.toFixed(2), amount: amount.toFixed(2)});
-    }
+  handlePrice(e) {
+    const price = e.target.value;
+    const commission = this.props.commission * e.target.value;
+    const amount = commission * this.state.actions / 1000;
+    this.setState({ price, commission: commission.toFixed(2), amount: amount.toFixed(2) });
+  }
 
-    handleActions(e) {
-        let actions = e.target.value,
-            commission = this.state.commission,
-            amount = actions * commission / 1000;
-        this.setState({actions: actions, commission: commission, amount: amount.toFixed(2)});
-    }
+  handleCommission(e) {
+    const commission = e.target.value;
+    const price = e.target.value / this.props.commission;
+    const amount = commission * this.state.actions / 1000;
+    this.setState({ commission, price: price.toFixed(2), amount: amount.toFixed(2) });
+  }
 
-    handleAmount(e) {
-        let amount = e.target.value,
-            commission = this.state.commission,
-            actions = amount / commission * 1000;
-        this.setState({amount: amount, commission: commission, actions: actions,});
-    }
+  handleActions(e) {
+    const actions = e.target.value;
+    const { commission } = this.state;
+    const amount = actions * commission / 1000;
+    this.setState({ actions, commission, amount: amount.toFixed(2) });
+  }
 
-    render() {
-        return (
-            <div className="Calculate">
-                <form>
-                    <label>Price:
-                        <input className="price" type="number" value={this.state.price} onChange={this.handlePrice} />
-                    </label>
-                    <label>Price with commission:
-                        <input className="commission" type="number" value= {this.state.commission} onChange={this.handleCommission} />
-                    </label>
-                    <label>Actions:
-                        <input className="action" type="number" value={this.state.actions} onChange={this.handleActions} />
-                    </label>
-                    <label>Total:
-                        <input className="total" type="number" value={this.state.amount} onChange={this.handleAmount} />
-                    </label>
-                </form>
+  handleAmount(e) {
+    const amount = e.target.value;
+    const { commission } = this.state;
+    const actions = amount / commission * 1000;
+    this.setState({ amount, commission, actions });
+  }
 
-                <div className="general-state">price: {this.state.price} <br /> comission: {this.state.commission} <br /> actions: {this.state.actions / 1000} <br /> amount: {this.state.amount}</div>
-            </div>
-        );
-    }
+  render() {
+    const { children } = this.props;
+    const props = this.getCalculateProps();
+    return (
+      <div>
+      {children(props)}
+      </div>
+    );
+  }
 }
 
 export default Calculate;
